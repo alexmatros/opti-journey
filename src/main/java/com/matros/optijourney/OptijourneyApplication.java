8230ec246121a1610b0d1eb58571df1de317b943
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-10-08 22:22:58 -0400

    Initial commit

---
+++ src/main/java/com/matros/optijourney/OptijourneyApplication.java
@@ -0,0 +1,13 @@
+package com.matros.optijourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptijourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptijourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matros/optijourney/OptijourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matros.optijourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptijourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
