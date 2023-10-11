e5fde369eb260a0b03c08245fdbdac0f2c1099e5
alexmatros <47996580+alexmatros@users.noreply.github.com>
2023-10-10 22:09:37 -0400

    Added google maps autocomplete to input fields

--- src/main/java/com/matros/optijourney/controllers/HomeController.java
+++ src/main/java/com/matros/optijourney/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.matros.optijourney.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,16 +12,31 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Value("${googlemaps.apikey}")
+    private String apiKey;
+
     @GetMapping("/")
     public String homepage(Model model) {
         model.addAttribute("numFields", 0);
+        model.addAttribute("apiKey", apiKey);
         return "homepage";
     }
 
     @PostMapping("/submit")
-    public String handleSubmit(@RequestParam List<String> textFields) {
-        // Process the form submission here
-        // textFields will contain a list of all the text field values
-        return "redirect:/map"; // Redirect back to the homepage
+    public String submitForm(
+            @RequestParam("origin") String origin,
+            @RequestParam("destination") String destination,
+            @RequestParam("waypoints") List<String> waypoints,
+            Model model) {
+
+        System.out.println("Origin: " + origin);
+        System.out.println("Destination: " + destination);
+        System.out.println("Waypoints: " + waypoints);
+
+        model.addAttribute("origin", origin);
+        model.addAttribute("destination", destination);
+        model.addAttribute("waypoints", waypoints);
+
+        return "map";
     }
 }
--- src/main/resources/static/js/addWaypoints.js
+++ src/main/resources/static/js/addWaypoints.js
@@ -1,22 +1,47 @@
+initAutocomplete();
+
 function addWaypoint() {
-    const numFields = document.querySelectorAll('input[type="text"]').length - 1;
+    const idx = document.querySelectorAll('input[name^="waypoints"]').length + 1;
     const div = document.createElement('div');
 
     const label = document.createElement('label');
-    label.setAttribute('for', 'waypoint' + numFields);
-    label.innerText = 'Waypoint ' + numFields + ': ';
+    label.setAttribute('for', 'waypoint' + idx);
+    label.innerText = 'Waypoint ' + idx + ': ';
     div.appendChild(label);
 
     const input = document.createElement('input');
     input.setAttribute('type', 'text');
-    input.setAttribute('id', 'waypoint' + numFields);
-    input.setAttribute('name', 'waypoint' + numFields);
+    input.setAttribute('id', 'waypoint' + idx);
+    input.setAttribute('name', `waypoints[${idx}]`);
+    input.setAttribute("autocomplete", "off");
     div.appendChild(input);
 
     const br1 = document.createElement('br');
-    const br2 = document.createElement('br');
     div.appendChild(br1);
+
+    const br2 = document.createElement('br');
     div.appendChild(br2);
 
     document.getElementById('waypoints').appendChild(div);
+
+    setInputAutocomplete(input);
+}
+
+function initAutocomplete() {
+    const originInput = document.querySelector('input[name="origin"]');
+    const destinationInput = document.querySelector('input[name="destination"]');
+
+    const options = {
+        componentRestrictions: {idxry: ['us', 'ca']} // Restrict to USA and Canada
+    };
+
+    var autocompleteOrigin = new google.maps.places.Autocomplete(originInput, options);
+    var autocompleteDestination = new google.maps.places.Autocomplete(destinationInput, options);
+
+    autocompleteOrigin.setFields(['place_id', 'name', 'formatted_address']);
+    autocompleteDestination.setFields(['place_id', 'name', 'formatted_address']);
+}
+
+function setInputAutocomplete(input) {
+    new google.maps.places.Autocomplete(input);
 }
